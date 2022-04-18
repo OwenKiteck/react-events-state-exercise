39ca9af0bdf63517d0d6db15ed41865bffcd2f3d
Student <owen.kiteck@stu.barren.kyschools.us>
2022-04-18 10:58:48 -0500

    Finished exercise and bonus

---
+++ src/bonus/Bonus.js
@@ -0,0 +1,18 @@
+import {useState} from "react";
+
+
+function Bonus(){
+    const variables = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+    const [letter, setLetter] = useState(0)
+    const update = () => setLetter(letter + 1)
+
+    return (
+        <div>
+            <h1>{variables[letter]}</h1>
+            <button onClick={ letter < 25 ? update : ''}>NEXT LETTER</button>
+        </div>
+    );
+}
+
+export default Bonus
